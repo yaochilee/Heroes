d340deb5ccd84eda87fefddcd94c40048b38eaf8
yaochilee <guybrush.106@gmail.com>
2022-01-03 17:25:52 +0800

    add message service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
     selector: 'app-heroes',
     templateUrl: './heroes.component.html',
@@ -11,7 +12,10 @@ export class HeroesComponent implements OnInit {
     // 所選的英雄
     selectedHero?: Hero;
 
-    constructor(private heroService: HeroService) {      
+    constructor(
+        private heroService: HeroService,
+        private messageService: MessageService
+    ) {
     }
 
     ngOnInit() {
@@ -19,11 +23,16 @@ export class HeroesComponent implements OnInit {
     }
 
     onClick(hero: Hero) {
-        this.selectedHero = hero;
+        
     }
 
     getHeroes(): void {
         this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    }
+
+    onSelect(hero: Hero) {
+        this.selectedHero = hero;
+        this.messageService.add(`HerosComponent: Selected hero id ${hero.id}`);
     }
 
 }
