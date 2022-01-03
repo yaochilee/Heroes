4325b1c975f68e7be1c4a2a0c8b1d6f0ad1923c5
yaochilee <guybrush.106@gmail.com>
2022-01-03 14:42:22 +0800

    add Hero interface

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    hero: Hero = {
+        id: 1,
+        name: "Iron Man"
+    }
+
 
 }
