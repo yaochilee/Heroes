45ed32c8e5c001e256e994849b850d836295145d
yaochilee <guybrush.106@gmail.com>
2022-01-05 11:33:24 +0800

    add goBack

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -26,4 +26,8 @@ export class HeroDetailComponent implements OnInit {
         this.heroService.getHero(id).subscribe(hero => this.hero = hero);
     }
 
+    goBack():void {
+        this.location.back();
+    }
+
 }
